6eb0d28347a24e79dfb2de1031da607fc567ebde
kwontaehan <109563379+kwontaehan@users.noreply.github.com>
2022-07-21 10:24:08 +0900

    Update main.py
    Ahri

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-
 import requests
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 url='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Aatrox.json'
+url2='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Ahri.json'
+
 res = requests.get(url).json()
+res2 = requests.get(url2).json()
 
 champ_lst = (res['data']['Aatrox']['key'],res['data']['Aatrox']['name'])
+champ_lst2 = (res['data']['Ahri']['key'],res['data']['Ahri']['name'])
